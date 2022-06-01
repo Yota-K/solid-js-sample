0cdcf7615ab765aa78e39f5180ee4891e52c67dc
Y.K <powdersugar828828@gmail.com>
2022-06-01 20:28:53 +0900

    feat: createSignalのsample書いてみて

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import type { Component } from 'solid-js';
+import { createSignal } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
 
+// できることはuseStateと一緒だが、solidjsにはhooksのルールがないので自由な場所に書けるみたい
+// stateは値ではなくgetter関数で返却される
+const [count, setCount] = createSignal(0);
+
 const App: Component = () => {
+  console.log('render')
   return (
     <div class={styles.App}>
       <header class={styles.header}>
@@ -11,6 +17,11 @@ const App: Component = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div style={{ display: 'flex'}}>
+          <button onClick={() => setCount(count() + 1)}>countUp</button>
+          <button onClick={() => setCount(0)}>reset</button>
+        </div>
+        {count()}
         <a
           class={styles.link}
           href="https://github.com/solidjs/solid"
